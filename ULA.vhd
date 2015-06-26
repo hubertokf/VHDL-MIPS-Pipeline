3e017da760875b6d4db87891664205f7f7440b8c
Huberto Kaiser Filho <hubertokf@gmail.com>
2015-06-26 00:01:00 -0300

    Fixed some instructions

--- ULA.vhd
+++ ULA.vhd
@@ -1,7 +1,6 @@
 Library IEEE;
 Use ieee.std_logic_1164.all;
 Use ieee.std_logic_unsigned.all;
-Use ieee.std_logic_arith.all;
 Use ieee.numeric_std.all;
 
 Entity ULA is
@@ -16,6 +15,7 @@ end ULA;
 architecture rtl of ULA is
 	signal sig_output: std_logic_vector(31 downto 0);
 begin
+
 	process(oper, in0, in1)
 	begin
 		case oper is
@@ -37,8 +37,24 @@ begin
 				sig_output <= (0 => '1', others => '0');
 			when "1000" =>
 				sig_output <= in0 xor in1;
-			--when "1001" =>
-				--sig_output <= in0 sll in1;
+			when "1001" =>
+				sig_output <= std_logic_vector(signed(in0) sll to_integer(signed(in1)));
+			when "1010" =>
+				sig_output <= std_logic_vector(signed(in0) srl to_integer(signed(in1)));
+			when "1011" =>
+				sig_output <= std_logic_vector(shift_right(signed(in0),to_integer(signed(in1))));
+			when "1100" =>
+				if (in0 < in1) then
+					sig_output <= "00000000000000000000000000000001";
+				else
+					sig_output <= "00000000000000000000000000000000";
+				end if;
+			when "1101" =>
+				if (unsigned(in0) < unsigned(in1)) then
+					sig_output <= "00000000000000000000000000000001";
+				else
+					sig_output <= "00000000000000000000000000000000";
+				end if;
 			WHEN OTHERS =>
 				sig_output <= in0;
 		end case;
